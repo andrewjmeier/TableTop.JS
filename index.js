4e7fb3b0e2b410c3572e430c1e98672a3c6e441e
Sudikoff Lab iMac <booyankees13@gmail.com>
2015-10-03 11:43:47 -0400

    starting to add game setup stuff

--- index.js
+++ index.js
@@ -1,23 +1,57 @@
 console.log("hello there")
 
-var test = {
-	escape: function(html) {
-		return String(html)
-			.replace(/&/g, '&amp;')
-			.replace(/"/g, '&quot;')
-			.replace(/'/g, '&#39;')
-			.replace(/</g, '&lt;')
-			.replace(/>/g, '&gt;');
+var Player = {
+	name: "",
+	money: 500,
+	properties: []
+};
+
+var john = Object.create(Player);
+john.name = "John";
+
+var steve = Object.create(Player);
+steve.name = "Steve";
+
+var sam = Object.create(Player);
+sam.name = "Sam";
+
+var players = [john, steve, sam];
+
+var Game = {
+	players: [],
+	currentPlayer: 0,
+	board: {},
+	dice: [],
+
+	randomizeCurrentPlayer: function() {
+		this.currentPlayer = Math.floor(Math.random() * this.players.length);
 	},
 
-	unescape: function(html) {
-		return String(html)
-			.replace(/&amp;/g, '&')
-			.replace(/&quot;/g, '"')
-			.replace(/&#39;/g, '\'')
-			.replace(/&lt;/g, '<')
-			.replace(/&gt;/g, '>');
+	rollDice: function(numberOfDice, sides) {
+		if (!sides) {
+			sides = 6;
+		}
+		this.dice = [];
+		for (i = 0; i < numberOfDice; i++) {
+			roll = Math.floor(Math.random() * sides) + 1;
+			this.dice.push(roll);
+		}
+	},
+
+	isDoubles: function(dice) {
+		return dice[0] === dice[1];
+	},
+
+	nextPlayer: function() {
+		if !this.isDoubles(this.dice) {
+			this.currentPlayer = (this.currentPlayer + 1) % this.players.length;
+		}
 	}
 };
 
-module.exports = test;
+var monopoly = Object.create(Game);
+
+monopoly.players = players;
+monopoly.randomizeCurrentPlayer();
+console.log(monopoly.currentPlayer);
+console.log(monopoly.rollDice(2));
