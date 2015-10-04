be88768f1bef631568becedf747525f3c081d623
konnorbeard <konnor.beard@gmail.com>
2015-10-04 13:54:28 -0400

    using constants for property groups

--- index.js
+++ index.js
@@ -28,6 +28,18 @@ var SHORTLINE_RR = 25;
 var ELECTRIC_CO = 26;
 var WATERWORKS = 27;
 
+// property groups
+var PG_BROWN = 0;
+var PG_LIGHT_BLUE = 1;
+var PG_PINK = 2;
+var PG_ORANGE = 3;
+var PG_RED = 4;
+var PG_YELLOW = 5;
+var PG_GREEN = 6;
+var PG_BLUE = 7;
+var PG_RR = 8;
+var PG_UTIL = 9;
+
 // placeholder for now - should talk when merged to properly integrate this with 
 // existing "board" and "players" implementations
 function Board(players) { 
@@ -211,43 +223,43 @@ function property_for_index(index, props) {
 */
 function properties_list() { 
   return [ 
-    ["Mediterranean Ave", "Dark Purple", 60, 50, [2, 10, 30, 90, 160, 250]], 
-    ["Baltic Ave", "Dark Purple", 60, 50, [4, 20, 60, 180, 320, 450]],
+    ["Mediterranean Ave", PG_BROWN, 60, 50, [2, 10, 30, 90, 160, 250]], 
+    ["Baltic Ave", PG_BROWN, 60, 50, [4, 20, 60, 180, 320, 450]],
 
-    ["Oriental Ave", "Light Blue", 100, 50, [6, 30, 90, 270, 400, 550]], 
-    ["Vermont Ave", "Light Blue", 100, 50, [6, 30, 90, 270, 400, 550]], 
-    ["Connecticut Ave", "Light Blue", 120, 50, [8, 40, 100, 300, 450, 600]], 
+    ["Oriental Ave", PG_LIGHT_BLUE, 100, 50, [6, 30, 90, 270, 400, 550]], 
+    ["Vermont Ave", PG_LIGHT_BLUE, 100, 50, [6, 30, 90, 270, 400, 550]], 
+    ["Connecticut Ave", PG_LIGHT_BLUE, 120, 50, [8, 40, 100, 300, 450, 600]], 
 
-    ["St. Charles Place", "Pink", 140, 100, [10, 50, 150, 450, 625, 750]], // 5
-    ["States Ave", "Pink", 140, 100, [10, 50, 150, 450, 625, 750]], 
-    ["Virginia Ave", "Pink", 160, 100 [12, 60, 180, 500, 700, 900]], 
+    ["St. Charles Place", PG_PINK, 140, 100, [10, 50, 150, 450, 625, 750]], // 5
+    ["States Ave", PG_PINK, 140, 100, [10, 50, 150, 450, 625, 750]], 
+    ["Virginia Ave", PG_PINK, 160, 100 [12, 60, 180, 500, 700, 900]], 
 
-    ["St. James Place", "Orange", 180, 100, [14, 70, 200, 550, 750, 950]], 
-    ["Tennessee Ave", "Orange", 180, 100, [14, 70, 200, 550, 750, 950]], 
-    ["New York Ave", "Orange", 200, 100, [16, 80, 220, 600, 800, 1000]], // 10
+    ["St. James Place", PG_ORANGE, 180, 100, [14, 70, 200, 550, 750, 950]], 
+    ["Tennessee Ave", PG_ORANGE, 180, 100, [14, 70, 200, 550, 750, 950]], 
+    ["New York Ave", PG_ORANGE, 200, 100, [16, 80, 220, 600, 800, 1000]], // 10
 
-    ["Kentucky Ave", "Red", 220, 150, [18, 90, 250, 700, 875, 1050]], 
-    ["Indiana Ave", "Red", 220, 150, [18, 90, 250, 700, 875, 1050]], 
-    ["Illinois Ave", "Red", 240, 150, [20, 100, 300, 750, 925, 1100]], 
+    ["Kentucky Ave", PG_RED, 220, 150, [18, 90, 250, 700, 875, 1050]], 
+    ["Indiana Ave", PG_RED, 220, 150, [18, 90, 250, 700, 875, 1050]], 
+    ["Illinois Ave", PG_RED, 240, 150, [20, 100, 300, 750, 925, 1100]], 
 
-    ["Atlantic Ave", "Yellow", 260, 150, [22, 110, 330, 800, 975, 1150]], 
-    ["Ventnor Ave", "Yellow", 260, 150, [22, 110, 330, 800, 975, 1150]], // 15
-    ["Marvin Gardens", "Yellow", 280, 150, [24, 120, 360, 850, 1025, 1200]], 
+    ["Atlantic Ave", PG_YELLOW, 260, 150, [22, 110, 330, 800, 975, 1150]], 
+    ["Ventnor Ave", PG_YELLOW, 260, 150, [22, 110, 330, 800, 975, 1150]], // 15
+    ["Marvin Gardens", PG_YELLOW, 280, 150, [24, 120, 360, 850, 1025, 1200]], 
 
-    ["Pacific Ave", "Green", 300, 200, [26, 130, 390, 900, 1100, 1275]], 
-    ["North Carolina Ave", "Green", 300, 200, [26, 130, 390, 900, 1100, 1275]], 
-    ["Pennsylvaniva Ave", "Green", 320, 200, [28, 150, 450, 1000, 1200, 1400]], 
+    ["Pacific Ave", PG_GREEN, 300, 200, [26, 130, 390, 900, 1100, 1275]], 
+    ["North Carolina Ave", PG_GREEN, 300, 200, [26, 130, 390, 900, 1100, 1275]], 
+    ["Pennsylvaniva Ave", PG_GREEN, 320, 200, [28, 150, 450, 1000, 1200, 1400]], 
 
-    ["Park Place", "Blue", 350, 200, [35, 175, 500, 1100, 1300, 1500]], // 20
-    ["Boardwalk", "Blue", 400, 200, [50, 200, 600, 1400, 1700, 2000]], 
+    ["Park Place", PG_BLUE, 350, 200, [35, 175, 500, 1100, 1300, 1500]], // 20
+    ["Boardwalk", PG_BLUE, 400, 200, [50, 200, 600, 1400, 1700, 2000]], 
 
-    ["Reading Railroad", "Railroad", 200, [25, 50, 100, 200]],
-    ["Pennsylvania Railroad", "Railroad", 200, [25, 50, 100, 200]],
-    ["B. & O. Railroad", "Railroad", 200, [25, 50, 100, 200]],
-    ["Shortline Railroad", "Railroad", 200, [25, 50, 100, 200]], // 25
+    ["Reading Railroad", PG_RR, 200, [25, 50, 100, 200]],
+    ["Pennsylvania Railroad", PG_RR, 200, [25, 50, 100, 200]],
+    ["B. & O. Railroad", PG_RR, 200, [25, 50, 100, 200]],
+    ["Shortline Railroad", PG_RR, 200, [25, 50, 100, 200]], // 25
 
-    ["Electric Company", "Utility", 150],
-    ["WaterWorks", "Utility", 150]
+    ["Electric Company", PG_UTIL, 150],
+    ["WaterWorks", PG_UTIL, 150]
   ];
 
 }
