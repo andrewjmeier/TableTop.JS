9e47fc861432c03f8981bbc4a8c365ea2f291aa3
konnorbeard <konnor.beard@gmail.com>
2015-11-15 18:13:16 -0500

    removing checker_token.js (not needed, just added color to token.js)

--- src/checkers/checker_token.js
+++
@@ -1,7 +0,0 @@
-var inherits = require('util').inherits;
-var Token = require("../token");
-
-function CheckerToken(owner, space, color, position) { 
-  Token.call(this, owner, space, color, position);
-} 
-
