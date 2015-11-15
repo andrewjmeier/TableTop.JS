3c7c68189fc1d8efca5b4773fe252b3a43bf70df
konnorbeard <konnor.beard@gmail.com>
2015-11-15 18:10:16 -0500

    work on framework default view class

--- src/view.js
+++ src/view.js
@@ -1,54 +1,52 @@
-var c = require("ttConstants.js");
-var GridBoard = require("gridboard");
+var c = require("./ttConstants.js");
+var GridBoard = require("./board/gridBoard.js");
 
 function View(game, turnMap) {
   this.game = game;
   this.turnMap = turnMap;
   this.turnMap.updateState("start");
+  this.tokenViews = [];
   this.tileViews = [];
+  this.boardView = new PIXI.Graphics();
+  
   if (game.board instanceof GridBoard) { 
-    for (var i = 0; i < this.game.board.height, i++) { 
+    for (var i = 0; i < this.game.board.height; i++) { 
       this.tileViews[i] = [];
     } 
   } 
-
-  this.tokenViews = [];
     
   this.renderer = PIXI.autoDetectRenderer(c.canvasWidth, c.canvasHeight,
                                           {backgroundColor : 0x1099bb});
-  
   document.body.appendChild(this.renderer.view);
-  
   // create the root of the scene graph
   this.stage = new PIXI.Container();
 };
 
+View.prototype.drawBoard = function() { 
+  if (this.game.board instanceof GridBoard) 
+    this.drawGridBoard();
+  
+  /* todo: 
+
+   else if (this.game.board instanceof PathBoard) 
+     this.drawPathBoard();
+   else if (this.game.board instanceof GraphBoard)
+     this.drawGraphBoard();
+   else 
+     do nothing and defer drawing to user? 
+
+   */
+};
+
 View.prototype.drawGridBoard = function() {
  
-  var board = new PIXI.Graphics();
   //board.lineStyle(1, 0x000000, 1);
-  board.beginFill(0xC2E2BF, 1);
-  board.drawRect(c.boardStartX, c.boardStartY, c.boardWidth, c.boardHeight);
-  this.stage.addChild(board);
+  this.boardView.beginFill(0xC2E2BF, 1);
+  this.boardView.drawRect(c.boardStartX, c.boardStartY, c.boardWidth, c.boardHeight);
+  this.stage.addChild(this.boardView);
   
-  var y_pos = 0;
-  var x_pos = 0;
-  if (!this.board.spaces[0]) return;
-  for (var x = 0; x < this.board.spaces[0].length; x++) {
-    x_pos = 0;
-    for (var y = 0; y < this.board.spaces.length; y++) {
-      var tileView = new PIXI.Graphics();
-      var tile = this.board.getSpace(x, y);
-      tileView.lineStyle(1, 0, 1);
-      tileView.drawRect(x_pos, y_pos, c.canvasWidth / this.game.board.spaces.length, c.canvasHeight);
-      x_pos += c.canvasWidth / this.game.board.spaces.length;
-      tileView.beginFill(tile.color, 1);
-      this.tileViews[x][y] = tileView;
-      this.stage.addChild(tileView);
-    }
-  }
-  
-  this.drawTokens();  
+  this.drawTiles(this.boardView);
+  this.drawTokens();
   this.drawMessage();
   this.animate();
 };
@@ -63,20 +61,70 @@ View.prototype.drawGraphBoard = function() {
 
 };
 
+View.prototype.drawTile = function(tile, boardView, x_pos, y_pos, tileWidth, tileHeight) {
+  var tileView = new PIXI.Graphics();
+  tileView.lineStyle(1, 0, 1);
+  tileView.drawRect(x_pos, y_pos, tileWidth, tileHeight);
+  tileView.beginFill(tile.color, 1);
+  
+  if (this.game.moveType == c.moveTypeManual) { 
+    tileView.interactive = true;
+    var context = this;
+    tileView.click = function(mouseData) { 
+      if (context.turnMap.getCurrentState() == "waitingForMove" && 
+          context.game.proposedMove.token) { 
+        context.game.setProposedMoveDestination(tile);
+        context.turnMap.updateState("makeMove");
+      };
+    };
+  } 
+  
+  this.tileViews[x_pos][y_pos] = tileView;
+  this.boardView.addChild(tileView);
+};
 
+View.prototype.drawTiles = function(boardView) {
+
+  var tileWidth = c.canvasWidth / this.game.board.spaces[0].length;
+  var tileHeight = c.canvasHeight / this.game.board.spaces.length;
+  var y_pos = 0;
+  var x_pos = c.canvasHeight;
+  
+  for (var x = 0; x < this.game.board.spaces[0].length; x++) {
+    x_pos = 0;
+    y_pos -= tileHeight;
+    for (var y = 0; y < this.game.board.spaces.length; y++) {
+      var tile = this.game.board.getSpace(x, y);
+      this.drawTile(tile, boardView, x_pos, y_pos, tileWidth, tileHeight);
+      x_pos += tile.width;
+    }
+  }
+};
+
+// draws token, puts it on appropriate tile,
+// adds it to tokenViews
 View.prototype.drawToken = function(token) {
 
   // draw the token 
   var tokenView = new PIXI.Graphics();
   tokenView.lineStyle(1, 0, 1);
   tokenView.beginFill(token.color, 1);
+  if (this.game.moveType == c.moveTypeManual) { 
+    tokenView.interactive = true;
+    var context = this;
+    tokenView.click = function(mouseData) { 
+      if (context.turnMap.getCurrentState() == "waitingForMove") { 
+        context.game.setProposedMoveToken(tokenView.token);
+      };
+    };
+  }
   
   // put it on the tile
   var tileView = this.tileViews[token.pos.x, token.pos.y];
   tokenView.drawRect(10, 10, 20, 20); // todo: other shapes
   tileView.addChild(tokenView);
 
-  this.tokenViews.push({tokenView: tokenView, tileView: tileView});
+  this.tokenViews.push({view: tokenView, token: token});
 };
 
 View.prototype.drawTokens = function() {
@@ -84,46 +132,51 @@ View.prototype.drawTokens = function() {
     var player = this.game.players[playerIdx];
     for (var tokenIdx in player.tokens) {
       var token = player.tokens[tokenIdx];
-      if (!token.isDead) {
-        this.updateToken(token.pos);
-      }
+      this.drawToken(token);
     }
   }
 };
 
-View.prototype.updateToken = function(token) {
-  var playerView = 
-  var token = playerView.token;
-
-  // remove the token from the previous tile
-  var previousTile = playerView.tile;
-  previousTile.removeChild(token);
-
-  // add the token as a child to the new tile
-  var currentTile = this.tileViews[position];
-  currentTile.addChild(token);
-  this.tokenViews[index] = {token: token, tile: currentTile};
-
-  // calculate an offset for the token if there are multiple
-  token.x = ((c.canvasWidth / this.game.board.spaces.length) * position);
+View.prototype.updateTokenView = function(tokenView) {
+  var view = tokenView.view;
+  var token = tokenView.token;
+  
+  // if it's dead, destroy and return
+  if (token.isDead) {
+    this.destroyTokenView(tokenView);
+    return;
+  } 
+  
+  // move the token to new tile
+  var destinationTile = this.tileViews[token.position.x][token.position.y];
+  destinationTile.removeChildren(); 
+  tokenView.view.setParent(destinationTile);
 };
 
 View.prototype.updateTokens = function() {
-  for (var playerIdx in this.game.players) { 
-    var player = this.game.players[playerIdx];
-    for (var tokenIdx in player.tokens) {
-      var token = player.tokens[tokenIdx];
-      if (!token.isDead) {
-        this.updateToken(token.pos);
-      }
-    }
-  }
+  var tokenView;
+  for (var tokenViewIdx in this.tokenViews) { 
+    tokenView = this.tokenViews[tokenViewIdx];
+    this.updateTokenView(tokenView);
+  } 
 };
-
-View.prototype.removeToken = function() { 
   
+View.prototype.destroyTokenViewForToken = function(token) { 
+  var tokenView;
+  for (var tokenViewIdx in this.tokenViews) { 
+    tokenView = this.tokenViews[tokenViewIdx];
+    if (tokenView.token == token) { 
+      this.destoryTokenView(tokenView);
+      return;
+    }
+  } 
 };
 
+View.prototype.destroyTokenView = function(tokenView) { 
+  tokenView.view.destroy();
+  this.tokenViews.remove(function(tv) { return tv == tokenView; });
+};
+  
 View.prototype.drawMessage = function() {
   var container = new PIXI.Container();
 
