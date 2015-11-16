bffbd4df89a4a2c43b0ab07537b947ce20cad8f1
konnorbeard <konnor.beard@gmail.com>
2015-11-16 01:22:59 -0500

    comments

--- src/checkers.js
+++ src/checkers.js
@@ -1,20 +1,26 @@
-var Player = require("./player.js");
+// Import necessary modules
+var Player = require("./player.js"); 
 var Checkers = require("./checkers/checker_game.js");
 var CheckerBoard = require("./checkers/checker_board.js");
 var ManualTurn = require("./manualTurn.js");
 var View = require("./view.js");
 
-
+// create the players
 var redPlayer = new Player({name: "Red"});
 var whitePlayer = new Player({name: "White"});
 var players = [redPlayer, whitePlayer];
 
+// create the Board, Game, and TurnMap
 var board = new CheckerBoard();
 var checkers = new Checkers(players, board);
 var turnMap = new ManualTurn(checkers);
 checkers.setTurnMap(turnMap);
 
-var view = new View(checkers, turnMap);
 
+// create our view, and draw it
+var view = new View(checkers, turnMap);
 view.drawBoard();
+
+// this initiates the TurnMap ("Gameloop") and 
+// gets the ball rolling!
 checkers.updateState("start");
